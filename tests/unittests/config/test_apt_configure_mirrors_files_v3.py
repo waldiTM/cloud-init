69311cf47ae6c50c185d6a3d28f8b1820da42d0e
Bastian Blank <waldi@debian.org>
2022-08-16 15:45:11 +0200

    config: Support files for apt-transport-mirror

---
+++ tests/unittests/config/test_apt_configure_mirrors_files_v3.py
@@ -0,0 +1,62 @@
+# This file is part of cloud-init. See LICENSE file for license information.
+
+""" test_apt_custom_mirrors_files
+Test creation of mirrors files
+"""
+import logging
+import os
+import shutil
+import tempfile
+from contextlib import ExitStack
+from unittest import mock
+from unittest.mock import call
+
+from cloudinit import subp, util
+from cloudinit.config import cc_apt_configure
+from cloudinit.distros.debian import Distro
+from tests.unittests import helpers as t_help
+from tests.unittests.util import get_cloud
+
+LOG = logging.getLogger(__name__)
+
+
+class TestAptSourceConfigMirrorsFiles(t_help.FilesystemMockingTestCase):
+    """TestAptSourceConfigMirrorsFiles - Class to test mirrors files rendering"""
+
+    def setUp(self):
+        super().setUp()
+        self.subp = subp.subp
+        self.new_root = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.new_root)
+
+        rpatcher = mock.patch("cloudinit.util.lsb_release")
+        get_rel = rpatcher.start()
+        get_rel.return_value = {"codename": "fakerel"}
+        self.addCleanup(rpatcher.stop)
+        apatcher = mock.patch("cloudinit.util.get_dpkg_architecture")
+        get_arch = apatcher.start()
+        get_arch.return_value = "amd64"
+        self.addCleanup(apatcher.stop)
+
+    def test_apt_v3_mirrors_list(self):
+        """test_apt_v3_mirrors_list"""
+        cfg = {"apt": {"enable_mirrors_files": True}}
+
+        mycloud = get_cloud("ubuntu")
+
+        with ExitStack() as stack:
+            mock_writefile = stack.enter_context(
+                mock.patch.object(util, "write_file")
+            )
+            stack.enter_context(
+                mock.patch.object(util, "ensure_dir")
+            )
+            cc_apt_configure.handle("test", cfg, mycloud, LOG, None)
+
+        mock_writefile.assert_has_calls([
+            call('/etc/apt/mirrors/ubuntu.list', 'http://archive.ubuntu.com/ubuntu/\n', mode=0o644),
+            call('/etc/apt/mirrors/ubuntu-security.list', 'http://security.ubuntu.com/ubuntu/\n', mode=0o644),
+        ])
+
+
+# vi: ts=4 expandtab
